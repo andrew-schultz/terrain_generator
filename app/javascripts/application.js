a999254e68d863cb3e8a946b73820d51c65d6b18
Andrew Schultz <as173171@gmail.com>
2018-02-11 22:20:47 -0500

    tinkered with pde file, added demo voice_changer

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -2,21 +2,22 @@ var headerElement = document.getElementById( 'mainHeader' );
 var newHeaderText = "Terrain Generator";
 headerElement.innerText = newHeaderText; 
 
-var audioPlayer = document.getElementById( 'audioPlayer' );
-var audioCtx = new ( window.AudioContext || window.webkitAudioContext )();
-var analyser = audioCtx.createAnalyser();
-var source = audioCtx.createMediaElementSource( audioPlayer );
-source.connect(analyser);
-source.connect( audioCtx.destination );
+// var audioPlayer = document.getElementById( 'audioPlayer' );
+// var audioCtx = new ( window.AudioContext || window.webkitAudioContext )();
+// var analyser = audioCtx.createAnalyser();
+// var source = audioCtx.createMediaElementSource( audioPlayer );
+// source.connect(analyser);
+// source.connect( audioCtx.destination );
 
-var frequencyData = new Uint8Array( analyser.frequencyBinCount );
+// var frequencyData = new Uint8Array( analyser.frequencyBinCount );
 
-function renderFrame() {
-   requestAnimationFrame( renderFrame );
-   // update data in frequencyData
-   analyser.getByteFrequencyData( frequencyData );
-   // render frame based on values in frequencyData
-   // console.log(frequencyData)
-}
+// function renderFrame() {
+//    requestAnimationFrame( renderFrame );
+//    // update data in frequencyData
+//    analyser.getByteFrequencyData( frequencyData );
+//    // render frame based on values in frequencyData
+//    // console.log(frequencyData)
+   
+// }
 
-renderFrame();
+// renderFrame();
